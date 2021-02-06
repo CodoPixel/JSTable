e9b2540e7fb0cb8a7db877d94cd6db6318903765
CodoPixel <gysemansthomas@hotmail.com>
2021-02-06 08:53:34 +0100

    3.0.0-alpha.3 (not working)
    rowspan & colspan

--- src/JSTable-v3.js
+++ src/JSTable-v3.js
@@ -42,8 +42,9 @@ class JSTable {
         return RegExp.$1.replace(/\(|\)/gm, '').split(',');
     }
 
-    generateCell(cell) {
+    generateCell(cell, colspan) {
         var cellElement = document.createElement('td');
+        var content = cell;
 
         // this.functions cannot be null because there is already <Main>
         for (var func of this.functions) {
@@ -52,19 +53,22 @@ class JSTable {
             // if there is a function inside the cell
             if (regex.test(cell)) {
                 // change the tagname if required
-                if (func.tagName) cellElement = document.createElement(func.tagName);
+                if (func.tagName) {
+                    cellElement = document.createElement(func.tagName);
+                }
 
                 // get the arguments if there is a callback function
                 if (func.callback) {
                     var args = this.getArgumentsFrom(func.name, cell);
-                    cellElement.innerHTML = func.callback(args);
-                    continue;
+                    content = func.callback(args);
+                } else {
+                    content = content.replace(regex, '');
                 }
             }
 
-            // otherwise
-            cell = cell.replace(regex, '');
-            cellElement.innerHTML = cell;
+            console.log(cellElement);
+            cellElement.setAttribute('colspan', colspan);
+            cellElement.innerHTML = content;
         }
         
         return cellElement;
@@ -72,10 +76,27 @@ class JSTable {
 
     jsArrayToHtml(arr) {
         var table = document.createElement('table');
-        for (var line of arr) {
+        for (var y = 0; y < arr.length; y++) {
             var tr = document.createElement('tr');
-            for (var cell of line) {
-                var cellElement = this.generateCell(cell);
+            for (var i = 0, r = 1, c = 1; i < arr[y].length; i++) {
+                if (arr[y][i] === "r.") continue;
+                if (arr[y][i] === "c.") continue;
+
+                var colspan = 1;
+                while (arr[y][i + r] === "r.") {
+                    colspan++;
+                    r++;
+                }
+
+                var rowspan = 1;
+                if (arr[y][i].indexOf("c.") !== -1) {
+                    while (arr[y + c][i] === "c.") {
+                        rowspan++;
+                        c++;
+                    }
+                }
+
+                var cellElement = this.generateCell(arr[y][i], colspan);
                 tr.appendChild(cellElement);
             }
             table.appendChild(tr);
